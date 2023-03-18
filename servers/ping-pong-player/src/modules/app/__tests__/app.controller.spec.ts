7adbd075f11d0b14fa9fa08f86debf5cd8d72bef
Patrick Da Silva <patrick.dasilva@polarsquad.com>
2023-03-18 03:49:45 +0100

    Added e2e test for /config

--- servers/ping-pong-player/src/modules/app/__tests__/app.controller.spec.ts
+++ servers/ping-pong-player/src/modules/app/__tests__/app.controller.spec.ts
@@ -2,11 +2,14 @@
 import { Test, TestingModule } from "@nestjs/testing"
 import { INestApplication } from "@nestjs/common"
 import * as request from "supertest"
+import { get as deepGet } from "lodash"
 
 // Global imports
+import { mockedConfig } from "src/modules/config/__mocks__/configService.mock"
 
 // Local imports
 import { AppModule } from "../app.module"
+import { LoadedConfigService } from "src/modules/config/config.service"
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -16,6 +19,7 @@ jest.mock("src/modules/config/loaders/yaml")
 
 describe("AppController (e2e)", () => {
 	let app: INestApplication
+	let loadedConfigService: LoadedConfigService
 
 	beforeEach(async () => {
 
@@ -26,14 +30,29 @@ describe("AppController (e2e)", () => {
 
 		app = moduleFixture.createNestApplication()
 		await app.init()
+
+		loadedConfigService = app.get<LoadedConfigService>(LoadedConfigService)
 	})
 
 	describe("/health (GET)", () => {
-		it("should return { status: 200 }", () => {
-			return request(app.getHttpServer())
+		it("should return { status: 200 }", async () => {
+			const response = await request(app.getHttpServer())
 				.get("/health")
 				.expect(200)
-				.expect({ status: 200 })
+
+			expect(response.body).toEqual({ status: 200 })
+		})
+	})
+
+	describe("/config (GET)", () => {
+		it("should return the mocked config object", async () => {
+			const response = await request(app.getHttpServer())
+				.get("/config")
+				.expect(200)
+
+			const config = loadedConfigService.getInstanceConfig()
+
+			expect(response.body).toEqual(config)
 		})
 	})
 })
