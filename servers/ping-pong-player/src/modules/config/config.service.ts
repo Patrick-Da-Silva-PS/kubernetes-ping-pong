489ec024baa71583e343bef08aee514ddfddb0a3
Patrick Da Silva <patrick.dasilva@polarsquad.com>
2023-03-18 01:10:42 +0100

    Added JSDoc to ConfigService and PlayService

--- servers/ping-pong-player/src/modules/config/config.service.ts
+++ servers/ping-pong-player/src/modules/config/config.service.ts
@@ -9,10 +9,20 @@ import type { ConfigType, NetworkConfig, PlayerConfig } from "src/modules/config
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * @class LoadedConfigService
+ * @category Config
+ * @description Service used to enable displaying the entire configuration setup in one callback.
+ */
 @Injectable()
 export class LoadedConfigService {
     constructor(private readonly configService: ConfigService) { }
 
+    /**
+     * @method getInstanceConfig
+     * @description Retrieves all the configuration registered in the ConfigModule.
+     * @returns {ConfigType} The entire configuration loaded in one object.  
+     */
     getInstanceConfig(): ConfigType {
         return {
             network: this.configService.get<NetworkConfig>("network"),
--- servers/ping-pong-player/src/modules/play/play.service.ts
+++ servers/ping-pong-player/src/modules/play/play.service.ts
@@ -9,12 +9,22 @@ import { nowTimestamp } from "src/utils"
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * @class PlayService
+ * @category Play
+ * @description Service used to determine the functionality and rules of the ping-pong game from the player's perspective.
+ */
 @Injectable()
 export class PlayService {
     private readonly logger = new Logger(PlayService.name)
 
     constructor(private readonly configService: ConfigService) { }
 
+    /**
+     * @method play
+     * @description Determines the behavior of the player when he receives a ball (i.e. a request from the ball generator). Logs its behavior for the log aggregator and returns its own ID for now. Behavior in the complete setting TBD.
+     * @returns 
+     */
     play(): string {
         this.logger.verbose(`Ping: ${nowTimestamp()}`)
         return this.configService.get<string>("player.playerID")
